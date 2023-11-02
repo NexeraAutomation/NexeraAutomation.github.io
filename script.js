fc81739341557a39d5a6db2f19895b98ea86d018
Henrik Mårtensson Etverk <henrik.martenssonetverk@gmail.com>
2023-11-02 22:34:39 +0100

    More translations.

--- script.js
+++ script.js
@@ -124,23 +124,23 @@ function showSlide(n) {
     switch (currentSlide) {
         case 1:
             slide_svg.innerHTML = "<path fill-rule='evenodd' d='M7.84 1.804A1 1 0 018.82 1h2.36a1 1 0 01.98.804l.331 1.652a6.993 6.993 0 011.929 1.115l1.598-.54a1 1 0 011.186.447l1.18 2.044a1 1 0 01-.205 1.251l-1.267 1.113a7.047 7.047 0 010 2.228l1.267 1.113a1 1 0 01.206 1.25l-1.18 2.045a1 1 0 01-1.187.447l-1.598-.54a6.993 6.993 0 01-1.929 1.115l-.33 1.652a1 1 0 01-.98.804H8.82a1 1 0 01-.98-.804l-.331-1.652a6.993 6.993 0 01-1.929-1.115l-1.598.54a1 1 0 01-1.186-.447l-1.18-2.044a1 1 0 01.205-1.251l1.267-1.114a7.05 7.05 0 010-2.227L1.821 7.773a1 1 0 01-.206-1.25l1.18-2.045a1 1 0 011.187-.447l1.598.54A6.993 6.993 0 017.51 3.456l.33-1.652zM10 13a3 3 0 100-6 3 3 0 000 6z' clip-rule='evenodd'></path>";
-            slide_h4.innerText = "Sänk kostnaderna";
-            slide_p.innerText = "Eliminera kostnader genom att överlåta uppgifter med låg avkastning till en AI-chattbot.";
+            slide_h4.innerText = "Reduce costs";
+            slide_p.innerText = "Eliminate costs by entrusting low-yielding tasks to an AI chatbot.";
             break;
         case 2:
           slide_svg.innerHTML = "<path fill-rule='evenodd' d='M5.75 2a.75.75 0 01.75.75V4h7V2.75a.75.75 0 011.5 0V4h.25A2.75 2.75 0 0118 6.75v8.5A2.75 2.75 0 0115.25 18H4.75A2.75 2.75 0 012 15.25v-8.5A2.75 2.75 0 014.75 4H5V2.75A.75.75 0 015.75 2zm-1 5.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h10.5c.69 0 1.25-.56 1.25-1.25v-6.5c0-.69-.56-1.25-1.25-1.25H4.75z' clip-rule='evenodd' fill='#FFFFFF'></path>";
-          slide_h4.innerText = "Öka hastigheten";
-          slide_p.innerText = "Chattboten ger omedelbara svar. Detta engagerar kunden och ökar sannolikheten för köp.";
+          slide_h4.innerText = "Increase velocity";
+          slide_p.innerText = "The chatbot gives instant answers. This engages the customer and increases the likelihood of purchase.";
           break;
         case 3:
           slide_svg.innerHTML = "<path d='M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z' fill='#FFFFFF'></path><path fill-rule='evenodd' d='M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z' clip-rule='evenodd' fill='#FFFFFF'></path>";
-          slide_h4.innerText = "Öka effektiviteten";
-          slide_p.innerText = "Ge ditt team de verktyg de behöver för att mångdubbla sina resultat.";
+          slide_h4.innerText = "Increase efficiency";
+          slide_p.innerText = "Give your team the tools they need to multiply their results.";
           break;
         case 4:
           slide_svg.innerHTML = "<path fill-rule='evenodd' d='M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z' clip-rule='evenodd' fill='#FFFFFF'></path>";
-          slide_h4.innerText = "Kvalitet";
-          slide_p.innerText = "Skapa konsekvent kvalitet genom att eliminera mänskliga fel och variationer.";
+          slide_h4.innerText = "Quality";
+          slide_p.innerText = "Create consistent quality by eliminating human errors and variations.";
           break;
     }
 
